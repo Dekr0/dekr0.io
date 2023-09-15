790997c23123ec7050ae8f13d94a15a2d6e0e458
Dekr0 <dekr0.dk@protonmail.com>
2023-09-15 17:04:24 -0600

    clean bun environemnt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import node from "@astrojs/node";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), react()],
-    experimental: { viewTransitions: true }
+    integrations: [react(), tailwind()],
+    output: "server",
+    adapter: node({
+        mode: "standalone"
+    })
 });
